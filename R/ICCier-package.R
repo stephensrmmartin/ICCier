563ff5ccd9f68291b1f9a785146f2bc2055851c3
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-04-23 17:43:15 -0700

    Initial Commit

---
+++ R/ICCier-package.R
@@ -0,0 +1,17 @@
+#' The 'ICCier' package.
+#' 
+#' @description A DESCRIPTION OF THE PACKAGE
+#' 
+#' @docType package
+#' @name ICCier-package
+#' @aliases ICCier
+#' @useDynLib ICCier, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import rstantools
+#' @importFrom rstan sampling
+#' 
+#' @references 
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#' 
+NULL
