8664a26d21ef12533f826139d57456ed35e2eec4
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-04-24 19:38:48 -0700

    Added skeleton for predict() and fitted().

---
+++ R/predict.R
@@ -0,0 +1,37 @@
+#' ICCier method to predict new ICC values.
+#'
+#' @param object ICCier object
+#' @param data Data to predict from. If NULL, calls \code{\link{fitted.ICCier}} on fit data.
+#' @param ... Not currently used.
+#'
+#' @return TBD
+#' @export
+#'
+predict.ICCier <- function(object, data, ...){
+ ## TODO
+}
+
+#' ICCier method to extract ICC values.
+#'
+#' @param object ICCier object
+#' @param ... Not currently used.
+#'
+#' @return TBD
+#' @export
+#'
+fitted.ICCier <- function(object){
+ ## TODO
+}
+
+#' ICCier method to predict new ICC values.
+#'
+#' @param object ICCier object
+#' @param data Data to predict from.
+#' @param ...
+#'
+#' @return TBD
+#' @keywords internal
+#'
+posterior_predict.ICCier <- function(object, data, ...){
+ ## TODO
+}
