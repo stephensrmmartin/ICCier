a82e57ce3f368a5cc813c1689515ea175ec6f046
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-05-01 15:57:19 -0700

    Removed unname() from print.summary. Unnecessary.

--- R/print.R
+++ R/print.R
@@ -124,7 +124,7 @@ print.summary.ICCier <- function(object,...){
   # colnames(object$ci$L$omega) <- rep(paste0((1-object$prob)/2*100,'%'),ncol(object$ci$L$omega))
   # colnames(object$ci$U$omega) <- rep(paste0((1 - (1-object$prob)/2)*100,'%'),ncol(object$ci$U$omega))
 
-  beta.sum <- unname(cbind(format(round(object$estimate$beta,digits),...),paste0('[',format(round(object$ci$L$beta,digits),...),' ',format(round(object$ci$U$beta,digits),...),']')))
+  beta.sum <- cbind(format(round(object$estimate$beta,digits),...),paste0('[',format(round(object$ci$L$beta,digits),...),' ',format(round(object$ci$U$beta,digits),...),']'))
   colnames(beta.sum) <- c('',paste0(object$prob*100,'%'))
   rownames(beta.sum) <- ''
 
